98b23222fd464fb42515a34460f446b2af785a07
pmbibe <53257014+pmbibe@users.noreply.github.com>
2023-12-08 17:01:09 +0700

    Update get_checklist.py

--- get_checklist.py
+++ get_checklist.py
@@ -4,10 +4,10 @@ import pathlib
 import re
 import json
 import pandas as pd
-# import xlsxwriter
 
 sql_scripts = []
 
+# find all .sql extension in directory
 def find_SQL(full_path):
     try:
         list_dir = pathlib.Path(full_path)
@@ -19,35 +19,21 @@ def find_SQL(full_path):
     except Exception as e:
         print(str(e))
 
-def get_statement(file_name):
-    statements = []
-    statement = ""
-    i = 0
-    # regex = r"^(CREATE|INSERT|SELECT|UPDATE|ALTER)"
-    regex = r"^(CREATE|INSERT|UPDATE|ALTER)"
-    regex2 = r".+;$"
+
+# print all script, include SQLPlus Command
+def read_checklist_script(file_name):
     with open(file_name, encoding="utf8") as f:
-        for line in f:
-            if re.match(regex, line.strip().split(" ")[0].upper()):                
-                if re.match(regex2, line.strip()):
-                    statements.append(line)
-                else:
-                    i=1
-            if not re.match(regex2, line.strip()) and i > 0 and "--" not in line.strip():
-                statement = statement + '{}'.format(line)
-            elif i > 0 and "--" not in line.strip():
-                statement = statement + '{}'.format(line)
-                statements.append(statement)
-                statement = ""
-                i = 0
-    return statements
-         
+        return f.read()    
+
+# get sub path 
 def sub_path_file(file_name, full_path):
     return format(pathlib.PureWindowsPath(file_name).as_posix().replace(full_path, ""))
 
+# get module name
 def module_name(file_name):
     return pathlib.PureWindowsPath(file_name).parts[6]
 
+# Get module and scripts's module with json format
 def deploy_rollback_backup(full_path):
     modules = []
     dict = {}
@@ -65,6 +51,7 @@ def deploy_rollback_backup(full_path):
             dict[module_name(sql_script_file)]["Deploy"].append("/".join(sub_path_file(sql_script_file, full_path).split("/")[1:]))
     return dict
 
+# Get module and scripts's module with excel format
 def deploy_rollback_backup_csv(full_path):
     modules = []
     dict = {}
@@ -82,16 +69,24 @@ def deploy_rollback_backup_csv(full_path):
             dict[module_name(sql_script_file)]["Deploy"]="\n".join(["/".join(sub_path_file(sql_script_file, full_path).split("/")[1:]),dict[module_name(sql_script_file)]["Deploy"]])
     return dict
 
+# Retrun json format with dictionary
 def json_format(dict):
     return json.dumps(dict, indent = 4) 
 
+# Get script name by index
 def get_file_by_index(index, dict):
     i = index.split(".")
-    i_module = int(i[0])
+    r = r"^\d+"
+    module_name = ""
+    for key in dict.keys():
+        if i[0] == re.findall(r, key)[0]:
+            i_module = list(dict.keys()).index(key)
+            module_name = key
     i_rbd = int(i[1])
     i_file = int(i[2])
-    return list(dict[list(dict)[i_module]][list(dict[list(dict)[i_module]])[i_rbd]])[i_file]
+    return "{}/{}".format(module_name, list(dict[list(dict)[i_module]][list(dict[list(dict)[i_module]])[i_rbd]])[i_file])
 
+# Set index for seraching script
 def set_file_index(dict):
     index_module = ""
     index_rbd = ""
@@ -108,8 +103,21 @@ def set_file_index(dict):
             index_rbd = ""
         index_module = ""
     return dict
-def dict_to_csv(dict):
-    df = pd.DataFrame.from_dict(dict)
-    writer = pd.ExcelWriter('Module.xlsx', engine='xlsxwriter')
-    df.style.set_properties(**{'vertical-align': 'top'}).to_excel(writer, sheet_name='Sheet1')
-    writer.close()
+
+# Retrun xlsx format with dictionary
+def dict_to_csv(dict, file_name):
+    try:
+        df = pd.DataFrame.from_dict(dict).transpose()
+        with pd.ExcelWriter('Module-{}.xlsx'.format(file_name), engine='xlsxwriter') as writer:
+            cell_format = writer.book.add_format()
+            cell_format.set_font_color('red')
+            cell_format.set_text_wrap()
+            cell_format.set_align('top')
+            df.to_excel(writer)
+            worksheet = writer.sheets["Sheet1"]
+            for col in range(len(df.columns)+1):
+                worksheet.set_column(col, col, None, cell_format)
+                worksheet.autofit()
+        print("Saved to XLSX File")
+    except Exception as e:
+        print(e)
