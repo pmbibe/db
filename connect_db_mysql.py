4eed568d7dadaf5a63d171e4fce13d67981c18b8
pmbibe <53257014+pmbibe@users.noreply.github.com>
2023-12-22 14:42:21 +0700

    Update connect_db_mysql.py

--- connect_db_mysql.py
+++ connect_db_mysql.py
@@ -4,18 +4,19 @@ import get_statements
 
 successed_script_file = []
 failed_script_file = []
-
+err = 0
 username = "pmbibe"
 password = 'Abc@12314455'
 host = "127.0.0.1"
 port = 3306
 db = "employees"
 
-
 def run_sql_script(file_path): 
     ddl_define_modify_scripts = []
     ddl_define_modify_scripts_vpf = []
     normal_scripts = []   
+    
+    
 
     # Group DDL statements implicit COMMIT: DDL statements implicit COMMIT include/exclude VIEW, PROCEDURE, FUNCTION, ... 
     def pre_run_sql_script(scripts):
@@ -36,7 +37,10 @@ def run_sql_script(file_path):
                 cursor.execute(script)
             except mysql.connector.Error as e:
                 print("Database Update Failed: {}".format(e))
+                failed_script_file.append(file_path)
+                break
         connection.close()
+        
 
     # Excute other scripts
     def normal_script(scripts):
@@ -48,18 +52,20 @@ def run_sql_script(file_path):
         for script in scripts:
             try:
                 cursor.execute(script)
-                successed_script_file.append(script)
             except mysql.connector.Error as e:
                 print("Database Update Failed: {}".format(e))
                 print("Script Error: {} in Script file: {}".format(script, file_path))
                 print("Rollback ...")
-                failed_script_file.append(script)
                 connection.rollback()
-        connection.commit()
+                if file_path not in failed_script_file: failed_script_file.append(file_path)
+                if file_path in successed_script_file: successed_script_file.remove(file_path)
+                break
 
+        connection.commit()
         # ENABLED AUTOCOMMIT
         connection.autocommit = True # type: ignore
         connection.close()
+        
 
     # Execute the SQL script
     sql_scripts = get_statements.get_statements(file_path)
@@ -67,11 +73,13 @@ def run_sql_script(file_path):
     ddl_define_modify_script(ddl_define_modify_scripts)
     ddl_define_modify_script(ddl_define_modify_scripts_vpf)
     normal_script(normal_scripts)
+    if file_path not in failed_script_file:
+        successed_script_file.append(file_path)
 
+    
 def connect_with_standalone():
     # Connect to the MySQL database
     connection = mysql.connector.connect(user=username, 
                                    password=password,
                                    host=host, port = port, database = db)
     return connection
-
