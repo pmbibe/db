203203266b3dff4ff7d121c7a99f7cf83160161d
pmbibe <53257014+pmbibe@users.noreply.github.com>
2023-12-27 16:56:44 +0700

    Update report.py

--- report.py
+++ report.py
@@ -17,7 +17,7 @@ def list2string(list):
 def dict_to_csv(dict, file_name, is_ver2):
     try:
         df = pd.DataFrame.from_dict(dict).transpose() if not is_ver2 else pd.json_normalize(dict).transpose()
-        with pd.ExcelWriter('/report/Module-{}.xlsx'.format(file_name), engine='xlsxwriter') as writer:
+        with pd.ExcelWriter('/report/{}.xlsx'.format(file_name), engine='xlsxwriter') as writer:
             cell_format = writer.book.add_format() # type: ignore
             cell_format.set_font_color('red')
             cell_format.set_text_wrap()
@@ -39,12 +39,15 @@ def create_report(all_scripts, module_name, sub_module_name, feature_name, is_st
             if is_in(module_name, file_path) and is_in(sub_module_name, file_path) and is_in(feature_name, file_path):
                 connect_db_cx.run_sql_script(file_path, is_standalone)
     except Exception as e:
-        print(str(e))                
-    successed_scripts = connect_db_cx.successed_script_file
-    failed_scripts = connect_db_cx.failed_script_file
-    successed_scripts = []  
-    failed_scripts = []       
-    not_run_yet_scripts = [x for x in all_scripts if x not in successed_scripts and x not in failed_scripts]
-    dict = {"All": list2string(all_scripts), "Successed": list2string(successed_scripts), "Failed": list2string(failed_scripts), "Not run yet": list2string(not_run_yet_scripts)}
-    dict_to_csv(dict, "Scripts", True)
+        print(str(e))          
+    if all_scripts != []:
+        successed_scripts = connect_db_cx.successed_script_file
+        failed_scripts = connect_db_cx.failed_script_file
+        successed_scripts = []  
+        failed_scripts = []       
+        not_run_yet_scripts = [x for x in all_scripts if x not in successed_scripts and x not in failed_scripts]
+        dict = {"All": list2string(all_scripts), "Successed": list2string(successed_scripts), "Failed": list2string(failed_scripts), "Not run yet": list2string(not_run_yet_scripts)}
+        dict_to_csv(dict, "{}-{}-{}".format(module_name, sub_module_name, feature_name), True)
+    else:
+        print("Not found module {} - sub-module {} - feature {}".format(module_name, sub_module_name, feature_name))
 
