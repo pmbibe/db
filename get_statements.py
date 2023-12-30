510f6e0475ad5d68638995577eb70b5043a60f3a
pmbibe <53257014+pmbibe@users.noreply.github.com>
2023-12-30 11:27:05 +0700

    Update get_statements.py

--- get_statements.py
+++ get_statements.py
@@ -33,24 +33,24 @@ def get_statements(file_name):
             # Check statement is inline or not
             if validate.validate_ddl_dml(line.strip().split(" ")[0].upper()) and (is_begin == 0 or is_if_case_for == 0):
                 if validate.validate_eos(line.strip()):
-                    statements.append(line.strip())
+                    statements.append(line)
                 else:
                     is_not_complete = True    
             # Complete statement if statement is not inline
 
             # Check line is end with ";" or not
             if not validate.validate_eos(line.strip()) and is_not_complete :
-                statement = "\n".join([statement,format(line.strip())])
+                statement = "\n".join([statement,format(line)])
 
             # Check line end with ";" but statement is not complete and line is in other line start with BEGIN
             elif not validate.validate_eos(line.strip()) and is_not_complete and (is_begin > 0 and begin_if_case_count > 0):
-                statement = "\n".join([statement,format(line.strip())])
+                statement = "\n".join([statement,format(line)])
 
             elif pl > 0 and validate.validate_eos(line.strip()) and is_not_complete and ( is_begin == 0 or (is_begin > 0 and begin_if_case_count > 0)):
-                    statement = "\n".join([statement,format(line.strip())])
+                    statement = "\n".join([statement,format(line)])
             elif  validate.validate_eos(line.strip()) and begin_if_case_count == 0 and is_not_complete :
-                statement = "\n".join([statement,format(line.strip())])
-                statements.append(statement.strip())
+                statement = "\n".join([statement,format(line)])
+                statements.append(statement)
                 statement = ""
                 is_not_complete = 0
                 pl = 0
