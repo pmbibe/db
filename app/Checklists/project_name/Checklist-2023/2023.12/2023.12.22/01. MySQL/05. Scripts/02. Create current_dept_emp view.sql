7813db1a3e96c656f2ec186378f8e89689aa865b
pmbibe <53257014+pmbibe@users.noreply.github.com>
2023-12-30 11:20:30 +0700

    Delete app/Checklists/project_name/Checklist-2023/2023.12/2023.12.22 directory

--- app/Checklists/project_name/Checklist-2023/2023.12/2023.12.22/01. MySQL/05. Scripts/02. Create current_dept_emp view.sql
+++
@@ -1,5 +0,0 @@
-CREATE OR REPLACE VIEW employees.current_dept_emp AS
-    SELECT l.emp_no, dept_no, l.from_date, l.to_date
-    FROM employees.dept_emp d
-        INNER JOIN employees.dept_emp_latest_date l
-        ON d.emp_no=l.emp_no AND d.from_date=l.from_date AND l.to_date = d.to_date;
