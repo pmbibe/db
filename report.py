85a8fa406d58693e0c2918c11bce6e42ae3cf36c
pmbibe <53257014+pmbibe@users.noreply.github.com>
2024-01-04 17:03:09 +0700

    Update report.py

--- report.py
+++ report.py
@@ -1,5 +1,6 @@
 # from pathlib import WindowsPath
 # import get_checklist
+import os
 import connect_db_cx
 import pandas as pd
 import validate
@@ -56,22 +57,35 @@ def read_file(file_name):
 
 def create_report(all_scripts, enum_folder, module_name, sub_module_name, feature_name, is_standalone):
     # all_scripts = [x.as_posix() for x in all_scripts]
-    all_scripts = [x.as_posix() for x in all_scripts if verify_input(module_name, sub_module_name, feature_name, x.as_posix()) ]
-    # write2file("{}/allScriptsFile".format(enum_folder), all_scripts)
-    
-    try:
-        for file_path in all_scripts:
-            if verify_input(module_name, sub_module_name, feature_name, file_path): 
-                print(file_path)
-                connect_db_cx.run_sql_script(file_path, is_standalone)
-    except Exception as e:
-        print(str(e))          
+    # all_scripts = [x.as_posix() for x in all_scripts if verify_input(module_name, sub_module_name, feature_name, x.as_posix()) ]
+    # if not os.path.exists("{}/allScriptsFile".format(enum_folder)): 
+    write2file("{}/allScriptsFile".format(enum_folder), [x.as_posix() for x in all_scripts if verify_input(module_name, sub_module_name, feature_name, x.as_posix()) ])
+    all_scripts = read_file("{}/allScriptsFile".format(enum_folder))
+    if not os.path.exists("{}/notRunYetScriptsFile".format(enum_folder)):
+        try:
+            for file_path in all_scripts:
+                if verify_input(module_name, sub_module_name, feature_name, file_path): 
+                    print(file_path.split('\n')[0])
+                    connect_db_cx.run_sql_script(file_path.split('\n')[0], is_standalone)
+        except Exception as e:
+            print(str(e))
+    else:
+        not_run_yet_scripts = read_file("{}/notRunYetScriptsFile".format(enum_folder))
+        try:
+            for file_path in not_run_yet_scripts:
+                
+                if verify_input(module_name, sub_module_name, feature_name, file_path): 
+                    print(file_path.split('\n')[0])
+                    connect_db_cx.run_sql_script(file_path.split('\n')[0], is_standalone)
+        except Exception as e:
+            print(str(e))
     if all_scripts != []:
-        # successed_scripts = read_file("{}/successedScriptFile".format(enum_folder))
-        successed_scripts = connect_db_cx.successed_script_file
-        failed_scripts = connect_db_cx.failed_script_file 
-        # failed_scripts = read_file("{}/failedScriptsFile".format(enum_folder))
-        not_run_yet_scripts = [x for x in all_scripts if x not in successed_scripts and x not in failed_scripts]
+        write2file("{}/successedScriptFile".format(enum_folder), connect_db_cx.successed_script_file)
+        write2file("{}/failedScriptsFile".format(enum_folder), connect_db_cx.failed_script_file)
+        successed_scripts = read_file("{}/successedScriptFile".format(enum_folder))
+        failed_scripts = read_file("{}/failedScriptsFile".format(enum_folder))
+        not_run_yet_scripts = [x for x in all_scripts if x not in successed_scripts]
+        write2file("{}/notRunYetScriptsFile".format(enum_folder), not_run_yet_scripts)
         dict = {"All": list2string(all_scripts), "Successed": list2string(successed_scripts), "Failed": list2string(failed_scripts), "Not run yet": list2string(not_run_yet_scripts)}
         dict_to_csv(dict, "{}-{}-{}".format(module_name, sub_module_name, feature_name), True)
     else:
